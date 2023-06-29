684179f8fa29d051752cd0b0460d2edc441f766c
Antonio Tenorio <victor.developer@outlook.com>
2023-06-29 18:01:10 -0300

    Initial Commit

---
+++ pointers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pointers
+//
+//  Created by Antonio Victor Tenorio on 29/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
